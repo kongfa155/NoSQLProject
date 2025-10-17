c43ae9ead0eaeda1c350e907e19a41bb5b1b18ff
Dao Cong Pha <mongpha90@gmail.com>
2025-10-17 12:55:45 +0700

    Fix đăng xuất

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,11 @@ import './NavBar.css';
 import { useNavigate } from 'react-router-dom';
 import webLogo from '../../quizLogo_green.svg';
 import { useSelector } from 'react-redux';
-
+import { useDispatch } from "react-redux";
+import { handleLogout } from "../../redux/action/userAction";
 
 export default function NavBar({selected="trangchu", isAdmin}){
-
+const dispatch = useDispatch();
 
       const account = useSelector((state) => state.user.account);
       const isAuthenticated = useSelector(
@@ -85,22 +86,24 @@ export default function NavBar({selected="trangchu", isAdmin}){
           <p className="my-2">ĐÓNG GÓP ĐỀ</p>
         </div>
         <div className="col-span-3"></div>
-        <div
-          className={`${
-            selected == "dangnhap" && "text-[#6ea269]"
-          } cursor-pointer duration-500
+        {isAuthenticated ? (
+          <div>
+            {" "}
+            <button onClick={() => handleLogout(dispatch)}>Đăng xuất</button>
+          </div>
+        ) : (
+          <div
+            className={`${
+              selected == "dangnhap" && "text-[#6ea269]"
+            } cursor-pointer duration-500
             transform transition ease-in-out  hover:scale-[1.15]`}
-          onClick={() => {
-            handleItemOnClick("canhan");
-          }}
-        >
-          {isAuthenticated ? (
-            <input type="dropbox">
-            </input>
-          ) : (
+            onClick={() => {
+              handleItemOnClick("canhan");
+            }}
+          >
             <p className="my-2">ĐĂNG NHẬP</p>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     );
 
