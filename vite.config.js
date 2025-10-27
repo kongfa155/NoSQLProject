45df40db9918d5e05d7261473ee29ee24212218c
nhhun2005 <nhhun2005@gmail.com>
2025-10-27 08:41:39 +0700

    no luc cuu con khoi benh ung thu phoi 3

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: command === "serve" ? "/" : "/NoSQLProject/",
   server: {
     host: "0.0.0.0",
     port: 5000,
@@ -18,4 +17,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
