44f27649612900650df242e66890c1e57f623e22
HuynhNui <nguyenhuynhngoquyen@gmail.com>
2025-11-20 23:05:52 +0700

    sua linh tinh 4

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -101,6 +101,7 @@ export const deleteUser = async (req, res) => {
     const user = await User.findByIdAndDelete(req.params.id);
     if (!user) return res.status(404).json({ message: "User not found" });
 
+  
     res.json({ message: "User deleted successfully" });
   } catch (err) {
     res.status(500).json({ message: err.message });
