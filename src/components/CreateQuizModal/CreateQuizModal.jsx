41c95ac16c249252472ab43ec658225d04b85dd7
Huan <nhhun2005@gmail.com>
2025-11-20 19:13:04 +0700

    fix size create subject modal

--- src/components/CreateQuizModal/CreateQuizModal.jsx
+++ src/components/CreateQuizModal/CreateQuizModal.jsx
@@ -114,8 +114,8 @@ export default function CreateQuizModal({
     return <></>;
   }
   return (
-    <div className="absolute inset-0 bg-black/50 flex items-center justify-center z-50 ">
-      <div className="relative w-[50%] h-[70%] bg-white rounded-[8px] shadow-black shadow-md pb-[96px] overflow-y-auto ">
+    <div className="absolute inset-0 bg-black/50 flex items-center justify-center z-9999 ">
+      <div className="relative w-[50%] h-[90%] bg-white rounded-[8px] shadow-black shadow-md pb-[96px] overflow-y-auto ">
         <div className="sticky top-0 right-0 flex justify-end p-4 bg-white z-10">
           <ExitIcon
             onClick={() => {
